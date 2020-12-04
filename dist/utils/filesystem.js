e8ac0c70f09712ff5fe8d1e0c10e5f822d975379
Bjarn Bronsveld <git@bjarn.net>
2020-12-04 16:43:34 +0100

    Fixed compatibility with Node <14

--- dist/utils/filesystem.js
+++ dist/utils/filesystem.js
@@ -3,7 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.ensureDirectoryExists = void 0;
 const tslib_1 = require("tslib");
 const fs_1 = require("fs");
-const promises_1 = require("fs/promises");
 /**
  * Ensure the given path exists, then return a string or false when failed.
  * @param path
@@ -12,7 +11,7 @@ function ensureDirectoryExists(path) {
     return tslib_1.__awaiter(this, void 0, void 0, function* () {
         if (!fs_1.existsSync(path)) {
             try {
-                yield promises_1.mkdir(path, { mode: 0o755 });
+                yield fs_1.mkdirSync(path, { mode: 0o755 });
                 return path;
             }
             catch (e) {
