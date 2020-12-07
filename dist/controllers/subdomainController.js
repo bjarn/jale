956073eb0596a271903a94c788687b34c12f3bfe
Bjarn Bronsveld <git@bjarn.net>
2020-12-07 22:58:02 +0100

    wip

--- dist/controllers/subdomainController.js
+++ dist/controllers/subdomainController.js
@@ -8,7 +8,7 @@ class SubdomainController {
     constructor() {
         this.appTypes = ['laravel', 'magento2', 'magento1'];
         this.execute = (option, subdomain) => tslib_1.__awaiter(this, void 0, void 0, function* () {
-            if (option != 'add' || option != 'del') {
+            if (option !== 'add' && option !== 'del') {
                 console.log('Invalid option. Please use \'add\' or \'del\', followed by the subdomain.');
                 return;
             }
--- src/controllers/subdomainController.ts
+++ src/controllers/subdomainController.ts
@@ -7,7 +7,7 @@ class SubdomainController {
     appTypes = ['laravel', 'magento2', 'magento1']
 
     execute = async (option: string, subdomain: string): Promise<void> => {
-        if (option != 'add' || option != 'del') {
+        if (option !== 'add' && option !== 'del') {
             console.log('Invalid option. Please use \'add\' or \'del\', followed by the subdomain.')
             return
         }
