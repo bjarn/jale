79ca7d8ce8952b0067a20c9824307d2f562b5178
Bjarn Bronsveld <git@bjarn.net>
2020-12-18 00:49:18 +0100

    Added tests for console utils

--- __tests__/console.spec.ts
+++ __tests__/console.spec.ts
@@ -1,4 +1,5 @@
-import {log} from '../src/utils/console'
+import kleur from 'kleur'
+import {info, log, success, verbose, warning, url, emptyLine, error, clearConsole} from '../src/utils/console'
 
 describe('Console Utils', () => {
     let outputData = ''
@@ -8,5 +9,70 @@ describe('Console Utils', () => {
         console['log'] = jest.fn(storeLog)
         log('prefix ', 'body')
         expect(outputData).toBe('prefix body')
+
+        outputData = ''
+    })
+
+    test('Verbose log', () => {
+        console['log'] = jest.fn(storeLog)
+        verbose('verbose message')
+        expect(outputData).toBe(kleur.gray(kleur.bold('🛠  ')) + kleur.gray('verbose message'))
+
+        outputData = ''
+    })
+
+    test('Info log', () => {
+        console['log'] = jest.fn(storeLog)
+        info('info message')
+        expect(outputData).toBe(kleur.gray(kleur.bold('✨  ')) + kleur.white('info message'))
+
+        outputData = ''
+    })
+
+    test('Success log', () => {
+        console['log'] = jest.fn(storeLog)
+        success('success message')
+        expect(outputData).toBe(kleur.gray(kleur.bold('\n✅ ')) + kleur.white('success message'))
+
+        outputData = ''
+    })
+
+    test('Warning log', () => {
+        console['log'] = jest.fn(storeLog)
+        warning('warning message')
+        expect(outputData).toBe(kleur.yellow(kleur.bold('⚠️  Warning: ')) + kleur.yellow('warning message'))
+
+        outputData = ''
+    })
+
+    test('Error log', () => {
+        console['log'] = jest.fn(storeLog)
+        error('error message')
+        expect(outputData).toBe(kleur.red(kleur.bold('\n🚨 Error: ')) + kleur.red('error message'))
+
+        outputData = ''
+    })
+
+    test('URL formatting', () => {
+        const result = url('https://github.com/bjarn/jale')
+        expect(result).toBe(kleur.bold(kleur.underline('https://github.com/bjarn/jale')))
+    })
+
+    test('Empty line log', () => {
+        console['log'] = jest.fn(storeLog)
+        emptyLine()
+        expect(outputData).toBe('')
+
+        outputData = ''
+    })
+
+    test('Clear console', () => {
+        console['log'] = jest.fn(storeLog)
+        process.stdout.rows = 10
+        const expectedLines = '\n'.repeat(10)
+        clearConsole()
+        expect(outputData).toBe(expectedLines)
+
+        outputData = ''
     })
 })
--- dist/__tests__/console.spec.js
+++ dist/__tests__/console.spec.js
@@ -1,5 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const tslib_1 = require("tslib");
+const kleur_1 = tslib_1.__importDefault(require("kleur"));
 const console_1 = require("../src/utils/console");
 describe('Console Utils', () => {
     let outputData = '';
@@ -8,6 +10,55 @@ describe('Console Utils', () => {
         console['log'] = jest.fn(storeLog);
         console_1.log('prefix ', 'body');
         expect(outputData).toBe('prefix body');
+        outputData = '';
+    });
+    test('Verbose log', () => {
+        console['log'] = jest.fn(storeLog);
+        console_1.verbose('verbose message');
+        expect(outputData).toBe(kleur_1.default.gray(kleur_1.default.bold('🛠  ')) + kleur_1.default.gray('verbose message'));
+        outputData = '';
+    });
+    test('Info log', () => {
+        console['log'] = jest.fn(storeLog);
+        console_1.info('info message');
+        expect(outputData).toBe(kleur_1.default.gray(kleur_1.default.bold('✨  ')) + kleur_1.default.white('info message'));
+        outputData = '';
+    });
+    test('Success log', () => {
+        console['log'] = jest.fn(storeLog);
+        console_1.success('success message');
+        expect(outputData).toBe(kleur_1.default.gray(kleur_1.default.bold('\n✅ ')) + kleur_1.default.white('success message'));
+        outputData = '';
+    });
+    test('Warning log', () => {
+        console['log'] = jest.fn(storeLog);
+        console_1.warning('warning message');
+        expect(outputData).toBe(kleur_1.default.yellow(kleur_1.default.bold('⚠️  Warning: ')) + kleur_1.default.yellow('warning message'));
+        outputData = '';
+    });
+    test('Error log', () => {
+        console['log'] = jest.fn(storeLog);
+        console_1.error('error message');
+        expect(outputData).toBe(kleur_1.default.red(kleur_1.default.bold('\n🚨 Error: ')) + kleur_1.default.red('error message'));
+        outputData = '';
+    });
+    test('URL formatting', () => {
+        const result = console_1.url('https://github.com/bjarn/jale');
+        expect(result).toBe(kleur_1.default.bold(kleur_1.default.underline('https://github.com/bjarn/jale')));
+    });
+    test('Empty line log', () => {
+        console['log'] = jest.fn(storeLog);
+        console_1.emptyLine();
+        expect(outputData).toBe('');
+        outputData = '';
+    });
+    test('Clear console', () => {
+        console['log'] = jest.fn(storeLog);
+        process.stdout.rows = 10;
+        const expectedLines = '\n'.repeat(10);
+        console_1.clearConsole();
+        expect(outputData).toBe(expectedLines);
+        outputData = '';
     });
 });
 //# sourceMappingURL=console.spec.js.map
