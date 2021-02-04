304d508cd0e0fd3c9153137569505f7264548e9d
Bjarn Bronsveld <git@bjarn.net>
2021-02-05 00:47:55 +0100

    Fixed secure and subdomain functionality

---
+++ dist/utils/regex.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.serverNamesRegex = void 0;
+exports.serverNamesRegex = new RegExp('(?<=server_name \\s*).*?(?=\\s*;)', 'gi');
+//# sourceMappingURL=regex.js.map
--- src/controllers/secureController.ts
+++ src/controllers/secureController.ts
@@ -6,6 +6,7 @@ import opensslConfig from '../templates/openssl'
 import {info, success, url, warning} from '../utils/console'
 import {ensureDirectoryExists} from '../utils/filesystem'
 import {getConfig, jaleSitesPath, jaleSslPath} from '../utils/jale'
+import {serverNamesRegex} from '../utils/regex'
 
 class SecureController {
 
@@ -21,12 +22,19 @@ class SecureController {
     constructor(project?: string) {
         this.config = getConfig()
         this.project = project || process.cwd().substring(process.cwd().lastIndexOf('/') + 1)
-        this.hostname = `${this.project}.${this.config.tld}`
 
-        this.keyPath = `${jaleSslPath}/${this.hostname}.key`
-        this.csrPath = `${jaleSslPath}/${this.hostname}.csr`
-        this.crtPath = `${jaleSslPath}/${this.hostname}.crt`
-        this.configPath = `${jaleSslPath}/${this.hostname}.conf`
+        const vhostConfig = readFileSync(`${jaleSitesPath}/${this.project}.conf`, 'utf-8')
+        const serverNames = serverNamesRegex.exec(vhostConfig)
+        this.hostname = `${this.project}.${this.config.tld}`
+        // TODO catch this issue
+        if (serverNames) {
+            this.hostname = serverNames[0].split(' ')[1]
+        }
+
+        this.keyPath = `${jaleSslPath}/${this.project}.key`
+        this.csrPath = `${jaleSslPath}/${this.project}.csr`
+        this.crtPath = `${jaleSslPath}/${this.project}.crt`
+        this.configPath = `${jaleSslPath}/${this.project}.conf`
     }
 
     executeSecure = async (): Promise<void> => {
@@ -47,10 +55,10 @@ class SecureController {
         if (await this.unsecure()) {
             success(`${this.hostname} has been unsecured and is no longer reachable over https.`)
             await (new Nginx()).restart()
-        } else {
-            warning(`The site ${this.hostname} is not secured.`)
-            return
         }
+
+        warning(`The site ${this.hostname} is not secured.`)
+        return
     }
 
     isSecure = (): boolean => {
@@ -76,9 +84,9 @@ class SecureController {
             this.unsecureNginxConfig()
 
             return true
-        } else {
-            return false
         }
+
+        return false
     }
 
     /**
@@ -110,7 +118,7 @@ class SecureController {
      * Make sure the Nginx config works with SSL.
      */
     private secureNginxConfig = () => {
-        let nginxConfig = readFileSync(`${jaleSitesPath}/${this.hostname}.conf`, 'utf-8')
+        let nginxConfig = readFileSync(`${jaleSitesPath}/${this.project}.conf`, 'utf-8')
         if (nginxConfig.includes('listen 443 ssl http2')) {
             // TODO: Implement a nicer check. This is just a rushed thing to prevent duplicate ssl entries. Maybe it's
             // fine, but I ain't so sure about that.
@@ -124,14 +132,14 @@ class SecureController {
     ssl_certificate ${this.crtPath};
     ssl_certificate_key ${this.keyPath};\n`)
 
-        writeFileSync(`${jaleSitesPath}/${this.hostname}.conf`, nginxConfig)
+        writeFileSync(`${jaleSitesPath}/${this.project}.conf`, nginxConfig)
     }
 
     /**
      * Clean up the Nginx config by removing references to the key en cert and stop listening on port 443.
      */
     private unsecureNginxConfig = () => {
-        let nginxConfig = readFileSync(`${jaleSitesPath}/${this.hostname}.conf`, 'utf-8')
+        let nginxConfig = readFileSync(`${jaleSitesPath}/${this.project}.conf`, 'utf-8')
 
         nginxConfig = nginxConfig.replace(`listen [::]:80;
     listen 443 ssl http2;
@@ -140,7 +148,7 @@ class SecureController {
     ssl_certificate ${this.crtPath};
     ssl_certificate_key ${this.keyPath};\n`, 'listen [::]:80;')
 
-        writeFileSync(`${jaleSitesPath}/${this.hostname}.conf`, nginxConfig)
+        writeFileSync(`${jaleSitesPath}/${this.project}.conf`, nginxConfig)
     }
 }
 
